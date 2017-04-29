6ca0c2653e8857c9e07c878c7cb7c33a6497ab49
Tony Nguyen <tntuyen@ucdavis.edu>
2017-04-29 02:58:50 -0700

    wired up search bar to API request and add lodash

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import MovieDetail from './components/Details';
 import axios from 'axios';
-import { fetchData } from './api';
+// import { fetchData } from './api';
 
 const API_KEY = "3d0f2b582ef98fc57bea7865717255e1"
-const rootURL = "https://api.themoviedb.org/3/movie/550?api_key="
-
-
+// const rootURL = "https://api.themoviedb.org/3/movie/550?api_key="
+// const rootURL = `https://api.themoviedb.org/3/search/movie?api_key=3d0f2b582ef98fc57bea7865717255e1&language=en-US&query=${search}&page=1&include_adult=false`
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {movie: null}
+		this.state = {movie: null, search: 'tarzan'}
+		this.onSearch = this.onSearch.bind(this);
 	}
 
+
+		fetchData() {
+			const rootURL = `https://api.themoviedb.org/3/search/movie?api_key=3d0f2b582ef98fc57bea7865717255e1&language=en-US&query=${this.state.search}&page=1&include_adult=false`
+
+			var url = rootURL
+
+			return axios.get(url)
+				.then(function(response) {
+					return response.data.results[0]
+			});
+		}	
+
 	componentWillMount() {
-		fetchData()
+		this.fetchData()
 			.then(function(data) {
 				// console.log(data);
 				this.setState(function() {
@@ -28,8 +40,29 @@ class App extends Component {
 			}.bind(this));
 	}
 
+	// shouldComponentUpdate() {
+	// 	this.fetchData()
+	// 		.then(function(data) {
+	// 			// console.log(data);
+	// 			this.setState(function() {
+	// 				return {
+	// 					movie: data
+	// 				}
+	// 			})
+	// 		}.bind(this));
+	// }
+
+	onSearch(term) {
+		this.setState({
+			search: term
+		})
+	}
+
+
+
 
   render() {
+  	console.log(this.state.search);
   	if(this.state.movie) {
 			var imgUrl = 'http://image.tmdb.org/t/p/w1280//' + this.state.movie.backdrop_path
 		}
@@ -41,10 +74,10 @@ class App extends Component {
   		backgroundPosition: 'center'
 		}
 
-  	console.log(this.state.movie);
+  	// console.log(this.state.movie);
     return (
       <div style={divStyle} className="container">
-        <SearchBar className="searchbar" />
+        <SearchBar className="searchbar" onSearch={this.onSearch} />
         <MovieDetail movieData={this.state.movie} />
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { searchTerm: '' }
+	}
+
+	onInputChange(term) {
+		this.setState({
+			searchTerm: term
+		})
+		this.props.onSearch(term)
+	}
+
 	render() {
+		// console.log(this.props)
 		return (
 			<div className="searchbar">
-				<input className="search" />
+				<input 
+				value={this.state.searchTerm}
+				onChange={(event) => this.onInputChange(event.target.value)}
+				className="search" />
 			</div>
 		)
 	}
